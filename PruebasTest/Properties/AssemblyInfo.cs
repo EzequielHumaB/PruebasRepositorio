3fbb54d0b07c090ae798105bb24a00a0757a2aa8
ezequiel huma brito <ezequielhuma@hotmail.com>
2019-06-22 18:40:37 -0400

    Profe, no se por que, pero cuando pruebo con diferentes pruebas el compilador siempre me dice que hay un tests, pero me he cuidado de hacer la prueba con todos

---
+++ PruebasTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PruebasTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PruebasTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bd83e770-4fe2-4417-a356-b0759525da05")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PruebasTest/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PatronRepositorioConPruebas.Repositorio;
+using PatronRepositorioConPruebas.Entidades;
+using PatronRepositorioConPruebas.DAL;
+namespace PruebasTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            RepositorioBase<Cargos> repositorio;
+            repositorio = new RepositorioBase<Cargos>();
+     
+           // Assert.IsTrue(repositorio.Guardar(new Cargos()));
+            Assert.IsTrue(repositorio.Guardar(new Cargos()));
+            //  Assert.IsTrue(repositorio.Modificar(new Personas())); 
+        }
+
+        public void textMethod1()
+        {
+            RepositorioBase<Proovedor> repositorio;
+            repositorio = new RepositorioBase<Proovedor>();
+
+            Assert.IsTrue(repositorio.Modificar(new Proovedor()));
+        }
+
+       
+    }
+}
